1f78900339c804bdebdb2967d8858713da716872
Johannes Maierhofer <johannes.maierhofer@bluestep.se>
2023-10-23 16:52:23 +0200

    Added error handling for consumer

--- Consumer/KeyPressedConsumer.cs
+++ Consumer/KeyPressedConsumer.cs
@@ -14,7 +14,49 @@ namespace Consumer
 
         public Task Consume(ConsumeContext<KeyPressed> context)
         {
-            _logger.LogInformation("Consuming KeyPressed event with key '{Key}'.", context.Message.Key);
+            var numberOfRetries = context.GetRetryAttempt();
+            _logger.LogInformation("Consuming KeyPressed event with key '{Key}' (RetryNo: {retries}).",
+                context.Message.Key,
+                numberOfRetries);
+            
+            // throw new Exception("Error for testing Consumer retries.");
+            return Task.CompletedTask;
+        }
+    }
+
+    public class KeyPressedDefinition : ConsumerDefinition<KeyPressedConsumer>
+    {
+        public KeyPressedDefinition()
+        {
+            ConcurrentMessageLimit = 8;
+        }
+
+        protected override void ConfigureConsumer(
+            IReceiveEndpointConfigurator endpointConfigurator,
+            IConsumerConfigurator<KeyPressedConsumer> consumerConfigurator)
+        {
+            consumerConfigurator.UseMessageRetry(r => r.Exponential(
+                3,
+                TimeSpan.FromSeconds(1),
+                TimeSpan.FromSeconds(5),
+                TimeSpan.FromSeconds(1)));
+        }
+    }
+
+    // consuming faults
+    // https://masstransit-project.com/usage/exceptions.html#consuming-faults
+    public class KeyPressedFault : IConsumer<Fault<KeyPressed>>
+    {
+        private readonly ILogger<KeyPressedFault> _logger;
+
+        public KeyPressedFault(ILogger<KeyPressedFault> logger)
+        {
+            _logger = logger;
+        }
+
+        public Task Consume(ConsumeContext<Fault<KeyPressed>> context)
+        {
+            _logger.LogInformation("Handle faulted KeyPressed event for message with Key '{Key}'.", context.Message.Message.Key);
             return Task.CompletedTask;
         }
     }
